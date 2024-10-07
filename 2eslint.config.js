7ceb92b2a55a819634c1e287b9b59c9da74fb995
francocastano <francomatiascastano@gmail.com>
2024-10-06 22:05:00 -0300

    Build cat fact page with custom React setup and Vite - Built a page that fetches and displays cat facts with images - Implemented a custom Vite setup using vanilla JavaScript and manual React integration - Displayed the first 3 words of the cat fact over the image

---
+++ 2eslint.config.js
@@ -0,0 +1,10 @@
+// export default [
+//     {
+//         "extends": "eslint:recommended",
+//         rules: {
+//             "indent": ["error", 4],
+//             "react/jsx-indent": ["error", 4],
+//             "react/jsx-indent-props": ["error", 4],
+//         }
+//     }
+// ];
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
+import React from 'react'
 import { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact"
-const CAT_ENDPOINT_IMAGE_URL = "https://catass.com/says/${}"
-
+const CAT_PREFIX_URL = "https://cataas.com"
 
 export function App() {
-
-  const [fact, setFact] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, sapiente?')
+  const [fact, setFact] = useState()
+  const [imageURL, setImageURL] = useState()
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
-      .then(response => response.json())
-      .then(data => setFact(data.fact))
+    async function getRandomFact() {
+      const response = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+      const randomFact = await response.json()
+      setFact(randomFact.fact)
+    }
+    getRandomFact()
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const threeWord = fact.split(' ').slice(0, 3).join(' ')
+
+    async function getCatImageURL() {
+      const response = await fetch(`${CAT_PREFIX_URL}/cat/says/${threeWord}?size=50&fontColor=red&json=true`)
+      const catImage = await response.json()
+      const { _id } = catImage
+      const catURL = `/cat/${_id}/says/${threeWord}?font=Impact&fontSize=30&fontColor=%23FFF&fontBackground=none&position=center`
+      setImageURL(catURL)
+    }
+    getCatImageURL()
+  }, [fact])
+
+
+
+  // T
+  // useEffect(() => {
+  //   async function getRandomFact() {
+  //     const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+  //     const json = await res.json()
+  //     setFact(json.fact)
+  //   }
+  //   getRandomFact()
+  // }, [])
+
 
   return (
     <main>
-      <h1>app de gatios</h1>
+      <h1>App de gatios</h1>
       {fact && <p>{fact}</p>}
+      {imageURL && <img src={`${CAT_PREFIX_URL}${imageURL}`} alt={`imgae extracted using the first three words for ${fact}`} />}
     </main>
   )
 
-  // Fact random: https://catfact.ninja/fact
-  // image randon: https://catass.com/says/hello
-  
 }
